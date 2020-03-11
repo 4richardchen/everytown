bf0111652d60104df916e6c1d1ae4dac3e8c19cb
Richard Chen <1322333+4richardchen@users.noreply.github.com>
2020-03-11 11:03:59 -0400

    handle form processing

--- rest-route.php
+++ rest-route.php
@@ -23,7 +23,18 @@ class RestApiRoutes {
     public function handle_contact_form(WP_REST_Request $request) {
 
         // logic for creating a new contact form submission
-        
+        $new_post_args = array(
+            'post_type' => 'contact',
+            'post_status' => 'publish',
+            'comment_status' => 'closed',
+            'ping_status' => 'closed',
+            'meta_input' => array(
+                'name' => $request->get_param('sender_name'),
+                'email' => $request->get_param('sender_email'),
+                //directions don't ask to store sender_message
+             ),
+        );
+        $new_post_id = wp_insert_post($new_post_args);
         return array('post_id' => $new_post_id );
     }
 
